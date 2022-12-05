d12e8c31dd2a6e3d1a0dae01d30a09a0aad75f69
Eden Sheiko <77243045+Eden-Sheiko@users.noreply.github.com>
2022-12-05 18:54:09 +0200

    Add files via upload

--- client_udp.c
+++ client_udp.c
@@ -1,82 +1,34 @@
+#include "client_udp.h"
 
-
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <inttypes.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <strings.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-
-#define BUF_SIZE 60
-#define IP_ADDRESS "192.168.1.2"
-#define FLAG_ZERO 0
-#define EQUAL_ZERO 0
-#define TRUE 1
-#define FALSE 0
-#define PORT 7
-#define LEN 30
-#define SLEEP_TIME 3
-#define ERROR -1
-#define MSG_BUFFER 256
-
-static unsigned int test_id = 0;
-
-typedef struct data_packet {
-    uint32_t Test_id;
-    uint8_t Peripheral;
-    uint8_t Iterations;
-    uint8_t length;
-    char msg[MSG_BUFFER];
-} data_packet_t;
-
-typedef struct res {
-    uint32_t Test_id;
-    uint8_t Test_result;
-} res_t;
-
-
-void udp_menu(uint8_t *per, uint8_t *it, uint8_t *leng, char *ms, uint32_t *test);
-
-void print_menu();
-
-void print_sub_menu();
+static unsigned int test_id = EQUAL_ZERO;
 
 int main(int argc, char *argv[]) {
-    while(1) {
-
+    while(TRUE) {
 
         /*************************************** Vars ***************************************************/
         struct sockaddr_in dest; // Holds Destination socket (IP+PORT)
         int socket_fd;           // Holds socket file descriptor
         unsigned int ssize;      // Holds size of dest
-        struct hostent *hostptr; // Holds host information
-        char buf[BUF_SIZE + 1];  // Used for writing/ Reading from buffer
         int retVal = EQUAL_ZERO;          // Holds return Value for recvfrom() / sendto()
-        char *cRetVal = NULL;    // Holds return Value for fgets()
-        data_packet_t data_packet;
-        res_t data_packet_answer;
-        udp_menu(&data_packet.Peripheral, &data_packet.Iterations, &data_packet.length, data_packet.msg,
-                 &data_packet.Test_id);
-
-
+        data_packet_t data_packet = {EQUAL_ZERO}; // creating instance of data_packet_t struct
+        res_t data_packet_answer = {EQUAL_ZERO}; // // creating instance of res_t struct
+        udp_menu(&data_packet.Peripheral, &data_packet.Iterations, &data_packet.length
+                 ,data_packet.msg,&data_packet.Test_id);
         /************************************* Initialization ******************************************/
 
+         //getting the size of the structs
         int i_size_of_struct = sizeof(data_packet);
         int i_size_of_new_struct = sizeof(data_packet_answer);
-        char buffer_to_send[i_size_of_struct];
 
+        //creating buffers with their size of the struct
+        char buffer_to_send[i_size_of_struct];
         char new_buffer[i_size_of_new_struct];
 
+        //setting the buffers to zero
         memset(buffer_to_send, EQUAL_ZERO, i_size_of_struct);
         memset(new_buffer, EQUAL_ZERO, i_size_of_new_struct);
 
+        // transforming the struct to buffer
         memcpy(buffer_to_send, &data_packet, i_size_of_struct);
 
 
@@ -98,17 +50,17 @@ int main(int argc, char *argv[]) {
         /************************************* Code *****************************************************/
 
 
-
-            retVal = sendto(socket_fd, &buffer_to_send, i_size_of_struct, FLAG_ZERO, (struct sockaddr *) &dest,
-                            ssize); // Send Ping
-            if (retVal < 0)
+        // Send Ping
+            retVal = sendto(socket_fd, &buffer_to_send, i_size_of_struct, FLAG_ZERO, (struct sockaddr *) &dest,ssize);
+            if (retVal < FALSE){
                 break;
+            }
 
-            retVal = recvfrom(socket_fd, &new_buffer, i_size_of_new_struct, FLAG_ZERO, (struct sockaddr *) &dest,
-                              &ssize); // Get answer
-            if (retVal < 0)
+        // Get answer
+            retVal = recvfrom(socket_fd, &new_buffer, i_size_of_new_struct, FLAG_ZERO, (struct sockaddr *) &dest,&ssize);
+            if (retVal < FALSE){
                 break;
-
+            }
 
             printf("------------------------------------\n");
             printf("             massage sent:      \n");
@@ -120,42 +72,45 @@ int main(int argc, char *argv[]) {
             printf("------------------------------------\n");
             printf("\n");
 
+            // getting the data
             data_packet_answer.Test_id = *(uint32_t *) (new_buffer + EQUAL_ZERO);
             data_packet_answer.Test_result = *(uint8_t *) (new_buffer + 4);
 
+
             printf("------------------------------------\n");
-            printf("              massage received:      \n");
+            printf("            massage received:      \n");
             printf("Test id : %d\n", data_packet_answer.Test_id);
-            printf("Test result (1 = passed 0 = failed)  : %d\n", data_packet_answer.Test_result);
+            printf("Test result : %d (1 = passed 0 = failed)  \n", data_packet_answer.Test_result);
             printf("------------------------------------\n");
             printf("\n");
 
-            sleep(3);
+            //Make the process sleep for 3 sec
+            sleep(SLEEP_TIME);
 
     }
 
 }
 
 void udp_menu(uint8_t *per, uint8_t *it, uint8_t *leng, char *ms, uint32_t *test) {
-    char c;
+    char char_holder;
     uint8_t peripheral = EQUAL_ZERO;
     uint8_t iter = EQUAL_ZERO;
     char msg[MSG_BUFFER] = {EQUAL_ZERO};
-    char *b = msg; // new
-    size_t bufsize = MSG_BUFFER; // new
-    size_t tmp = EQUAL_ZERO; // new
-    int k; // new
+    char *ptr_c = msg;
+    size_t buffer_size = MSG_BUFFER;
+    size_t data_holder = EQUAL_ZERO;
+    int int_holder;
     uint8_t len = EQUAL_ZERO;
 
-
-   print_menu();
-
+    // call to the menu
+    print_menu();
+    // loop to get user input
     while (1) {
         fflush(stdin);
         fflush(stdout);
-        scanf(" %c", &c);
+        scanf(" %char_holder", &char_holder);
         print_sub_menu();
-        switch (c) {
+        switch (char_holder) {
             case 'a':
                 printf("please enter an Peripheral \n");
                 scanf("%hhu", &peripheral);
@@ -168,15 +123,15 @@ void udp_menu(uint8_t *per, uint8_t *it, uint8_t *leng, char *ms, uint32_t *test
                 break;
             case 'c':
                 printf("please enter a string to send \n");
-                while ((k = getchar()) != '\n' && k != EOF);
-                tmp = getline(&b,&bufsize,stdin);
+                while ((int_holder = getchar()) != '\n' && int_holder != EOF);
+                data_holder = getline(&ptr_c, &buffer_size, stdin);
 
-                strcpy(ms, b);
-                len = tmp+1;
+                strcpy(ms, ptr_c);
+                len = data_holder + BYTE;
                 *leng = len;
                 break;
             case 'd':
-                printf("sending.. \n");
+                printf("sending... \n");
                 ++test_id;
                 *test = test_id;
                 return;
